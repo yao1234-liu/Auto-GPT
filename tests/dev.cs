469f9fff972822efaeecafd5704b485e2035eab6
姚 刘通 <2973134737@qq.com>
2023-05-19 17:11:58 +0800

    test

--- tests/dev.cs
+++ tests/dev.cs
@@ -2,9 +2,13 @@
 using System.Threading;
 public class Class1
 {
+	/// <summary>
+    /// 
+    /// </summary>
+    /// <param name=""></param>
 	public Class1(log)
 	{
-		
+	
 		var st = DateTime.Now.ToString();
 		Thread.Sleep(3000);
 	}
